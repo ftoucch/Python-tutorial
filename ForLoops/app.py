a9eb0ffda92455bf6f9c2cd311eecf27d5895fc0
faith-alimi <faith.alimi@juicymedia.co.uk>
2024-09-08 22:07:55 +0100

    class continuation

---
+++ ForLoops/app.py
@@ -0,0 +1,5 @@
+prices = [10, 20, 30]
+total_price = 0
+for price in prices :
+    total_price += price
+print (f"Total: {total_price}")
---
+++ List/app.py
@@ -0,0 +1,7 @@
+numbers = [1,4,7,8,9]
+max=numbers[0]
+
+for number in numbers:
+    if number > max:
+        max = number
+print(max)
