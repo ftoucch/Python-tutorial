f7f97b079c3573e8e100d0cdca4fdff9ccc76012
faith-alimi <faith.alimi@juicymedia.co.uk>
2024-09-21 18:55:57 +0100

    7hours python course

--- 7Hours Django course/studybud/base/admin.py
+++ 7Hours Django course/studybud/base/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Room, Topic, Message
 
-# Register your models here.
+class RoomDisplay(admin.ModelAdmin):
+    list_display = ('host', 'name', 'description', 'topic')
+
+
+class TopicDisplay(admin.ModelAdmin):
+    list_display = ('id','name')
+
+
+class MessageDisplay(admin.ModelAdmin):
+    list_display=('user', 'room', 'body')
+
+admin.site.register(Room,RoomDisplay)
+admin.site.register(Topic, TopicDisplay)
+admin.site.register(Message, MessageDisplay)
